dd97580a8d4353c75d59c3678c3153c6717a2b33
oli-kis <o.kistler.inf21@stud.bbbaden.ch>
2026-04-09 22:06:23 +0200

    url

--- TournamentAppBackend/Program.cs
+++ TournamentAppBackend/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddScoped<IStandingService, StandingService>();
 builder.Services.AddScoped<IMatchGenerationService, MatchGenerationService>();
 builder.Services.AddScoped<IKnockoutService, KnockoutService>();
 
-var originURL = builder.Environment.IsDevelopment() ? "http://localhost:8080" : "https://fc-frick-tournament.vercel.app/";
+var originURL = builder.Environment.IsDevelopment() ? "http://localhost:8080" : "https://fc-frick-tournament.vercel.app";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
